99b37b465eb9cd3c722f1ab9d130cc7260ae5f82
Daniel C <65579510+UnrealOG@users.noreply.github.com>
2020-11-19 20:32:44 -0500

    Files should be up to date now.
    Added tachometer, camera feed in middle, throttle position text. Moved speed and rpm texts to bottom corners.

--- main.py
+++ main.py
@@ -5,24 +5,43 @@ from tkinter import *
 import time
 import random
 import speedometer
+import tachometer
+from cv2 import *
+from PIL import Image, ImageTk
 
-#Tkinter, initialize all variables
+WIDTH,HEIGHT=1024,576
+
+'''Tkinter, initialize all variables'''
+
 root=Tk()
-screen=Canvas(root,height=576,width=1024)
+
+#Video capture
+video=cv2.VideoCapture(0)
+video.set(cv2.CAP_PROP_FRAME_WIDTH,300)
+video.set(cv2.CAP_PROP_FRAME_HEIGHT,200)
+
+screen=Canvas(root,height=HEIGHT,width=WIDTH)
 speedtext = Label(text="0")
 rpmtext=Label(text='0',fg="black")
-speedtext.config(font=("Arial",42))
+throttletext=Label(text='0')
+speedtext.config(font=("Arial",60))
 rpmtext.config(font=("Arial",60))
-speedtext.place(rely=0.43,relx=0.132,anchor='w')
+throttletext.config(font=("Arial",40))
+speedtext.place(rely=0.9,relx=0.0,anchor='w')
 rpmtext.place(rely=0.9,relx=1.0,anchor='e')
+throttletext.place(relx=0.5,rely=0.15,anchor='n')
 screen.pack()
 screen.create_oval(30,30,300,300,tag="oval")
-screen.create_oval(1000,30,720,300,tag="tachoval")
-A=speedometer.Speedometer(screen,"oval",Range=(40,0)) #speedometer
-#B=speedometer.Speedometer(screen,"tachoval",Range=(7,0))  #tachometer
-#for next week, look at speedometer
-#If that is easy, look into open webcam using tkinter for webcam feed
+screen.create_oval(1000,30,720,310,tag="tachoval")
+A=speedometer.Speedometer(screen,"oval",Range=(40,0))
+B=tachometer.Tachometer(screen,"tachoval",Range=(7000,0))
 
+#Label for camera spot
+lmain=Label(root)
+lmain.place(relx=0.5,rely=0.5,anchor='c')
+
+
+#Week 1
 '''
 Camera in the center.
 speedometer on the left
@@ -31,30 +50,59 @@ Right side - digital number readout
 left side - engine rpm. lower left - number. upper left - dial
 right side - miles per hour. Lower right - number. Upper right - dial'''
 
+#Week 2
 '''
 Try to get webcam feed
 Or work on tachometer
 Only show every other number to make it easier to read
 '''
 
-testSpeed=1
-#testSpeed = int(input())
-#print('speed:' +str(testSpeed))
+#Week 3
+'''
+Put webcam feed into tkinter window
+
+Will eventually work on interfacing with arduino
+'''
+
+#Week 4
+'''
+Main focus:
+    Fix speedometer
+    Learn about serial communication
+
+Other:
+Log all of it to a csv file.
+    As rpm is being displayed
+    timestamp: engine speed, vehicle speed
+    engine rpm as function of time
+    
+'''
+
+releasedVideo=False
+testSpeed=0
+tachSpeed=0
 def getSpeed(increment):
     return int(20+increment)
 
-
 #def updateLabel(speedInput):      #Input for speed goes here
 while(True):
-    time.sleep(0.9)
-    speedtext.config(text=str(testSpeed))
-    rpmtext.config(text=str(random.randint(0,75))+" rpm")
-    A.moveto(testSpeed*1.05,"oval")
-    #B.moveto(testSpeed*1.05,"tachoval")
+    _, frame=video.read()
+    frame=cv2.flip(frame,1)
+    cv2image=cv2.cvtColor(frame,cv2.COLOR_BGR2RGBA)
+    img=Image.fromarray(cv2image)
+    imgtk=ImageTk.PhotoImage(image=img)
+    lmain.imgtk=imgtk
+    lmain.configure(image=imgtk)
+    time.sleep(0.0333)
+    speedtext.config(text=str(testSpeed)+' mph')
+    rpmtext.config(text=str(tachSpeed)+" rpm")
+    throttletext.config(text="Throttle: "+str(random.randint(0,100)))
+    A.moveto(testSpeed,"oval")
+    B.moveto(tachSpeed,"tachoval")
     screen.update()
+    tachSpeed+=100
     testSpeed+=1
 
+video.release()
 
-
-#updateLabel(44)
-#screen.mainloop()
+screen.mainloop()
--- speedometer.py
+++ speedometer.py
@@ -6,7 +6,7 @@ class RadiusError(Exception):
 class Speedometer():
     def changerange(self,Range=(-10,190),rfont=("Verdana",34)):
         for i in range(-10,195,10):
-            self.canvas.itemconfig(self.range_marks[int((i+10)/10)],text=str((i+10)*(Range[1]-Range[0])/200+Range[0]),font=rfont)                        
+            self.canvas.itemconfig(self.range_marks[int((i+10)/10)],text=str((i+10)*(Range[1]-Range[0])/Range[0]),font=rfont)                        
     def moveto(self,value,tag):
         #if(value>self.range[1]):
             #value=self.range[1]
@@ -23,7 +23,7 @@ class Speedometer():
         #y: self.radius-self.radius*m.sin((value*self.slope+self.intercept)*m.pi/180)+30
 
     #needle        
-    def __init__(self,canvas,tag,bg="#ffffff",needlecolor="#0d47a1",markscolor="#000000",Range=(0,200),digitscolor="#ff2222"):
+    def __init__(self,canvas,tag,bg="#ffffff",needlecolor="#0d47a1",markscolor="#000000",Range=(0,30),digitscolor="#ff2222"):
         self.range=Range
         self.needlecolor=needlecolor
         self.canvas=canvas
@@ -33,21 +33,22 @@ class Speedometer():
             #raise RadiusError
         #else:
         self.radius=(temp[2]-temp[0])/2
-        self.slope=190.0/(self.range[1]-self.range[0])
+        self.slope=200.0/(self.range[1]-self.range[0])
         self.intercept=190-self.range[1]*self.slope     #was 190 before
         self.range_marks=[]
         q=10
         u=0
-        for i in range(-10,195,5):
+        #Speed markings
+        for i in range(-10,195,20):
             if(i%10==0):
                 #Main speed markings
                 x1,y1=self.radius+(self.radius-20)*m.cos(i*m.pi/180)+30,self.radius-(self.radius-20)*m.sin(i*m.pi/180)+30
                 canvas.create_line(x1,y1,self.radius+self.radius*m.cos(i*m.pi/180)+30,self.radius-self.radius*m.sin(i*m.pi/180)+30,fill=markscolor,width=2) 
-                self.range_marks.append(canvas.create_text(x1+u,y1+q,text=int((i+10)*(self.range[1]-self.range[0])/200+self.range[0]),font=("Courier",int(self.radius/10)),fill=digitscolor))
+                self.range_marks.append(canvas.create_text(x1+u,y1+q,text=int((i+10)*(self.range[1]-self.range[0]-20)/300+self.range[0]),font=("Courier",int(self.radius/10)),fill=digitscolor))
             else:
                 #In between markings
                 x1,y1=self.radius+(self.radius-10)*m.cos(i*m.pi/180),self.radius-(self.radius-10)*m.sin(i*m.pi/180)
-                canvas.create_line(x1+30,y1+30,self.radius+self.radius*m.cos(i*m.pi/180)+30,self.radius-self.radius*m.sin(i*m.pi/180)+30,fill="#acace6",width=2)
+                canvas.create_line(x1,y1,self.radius+self.radius*m.cos(i*m.pi/180)+30,self.radius-self.radius*m.sin(i*m.pi/180)+30,fill="#acace6",width=2)
             if(i>=90):
                 q=+10
                 u=q
